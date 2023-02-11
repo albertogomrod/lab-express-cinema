4cfdc6a6a92fd2a63483de628fb3ca6db9bef3e8
Alberto Gómez <albertogomrod5@gmail.com>
2023-02-11 15:31:16 +0100

    Lab hecho sin CSS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const MovieModel = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .then((response) => {
+      console.log(response);
+
+      res.render("movies.hbs", {
+        allMovies: response,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+
+
+  MovieModel.findById(req.params.id)
+    .then((response) => {
+      res.render("details.hbs", {
+        movieInfo: response,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
